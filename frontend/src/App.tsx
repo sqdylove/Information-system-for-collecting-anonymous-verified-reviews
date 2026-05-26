3b0b6953be3d6dd4831d448bef9821c9c3a91b8a
alexandr b. <alexandr.bryaginya@gmail.com>
2026-05-27 01:22:28 +0700

    bugfix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import AdminScreen from "./components/screens/AdminScreen";
 import MainScreen from "./components/screens/mainScreen";
 
 function App() {
+  const [screen, setScreen] = useState<"main" | "sender" | "recipient">("main");
+
   const isElectron =
     typeof window !== "undefined" &&
     navigator.userAgent.toLowerCase().includes("electron");
   if (isElectron) {
-    return <AdminScreen />;
+    return <AdminScreen setScreen={setScreen}/>;
   } else {
     return <MainScreen />;
   }
