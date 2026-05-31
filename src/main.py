a2a0ae32295d8ed1d212f20425b0e314387170b4
alexandr b. <alexandr.bryaginya@gmail.com>
2026-06-01 02:48:19 +0700

    cors fix #2.1

--- src/main.py
+++ src/main.py
@@ -12,8 +12,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
-    allow_headers=["Authorization", "Content-Type", "Accept"], 
+    allow_headers=["Authorization", "Content-Type", "Accept"],
 )
 app.include_router(box_router.router)
 app.include_router(feedback_router.router)
